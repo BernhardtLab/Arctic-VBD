ced0846165376abc365751bbfc584f916aff9640
Lilian Chan <tszyinglilianchan@gmail.com>
2025-04-09 11:36:54 -0400

    Upload files

---
+++ R-scripts/00_Functions.R
@@ -0,0 +1,56 @@
+setwd("~/Library/CloudStorage/OneDrive-UniversityofGuelph/Grad School/Chapter 1")
+
+# Briere model (truncated)
+sink("briere_T.txt")
+cat("
+    model{
+
+    ## Priors
+    cf.T0 ~ dunif(0, 20)
+    cf.Tm ~ dunif(20, 45)
+    cf.q ~ dunif(0, 1)
+    cf.sigma ~ dunif(0, 1000)
+    cf.tau <- 1 / (cf.sigma * cf.sigma)
+
+    ## Likelihood
+    for(i in 1:N.obs){
+    trait.mu[i] <- cf.q * temp[i] * (temp[i] - cf.T0) * sqrt((cf.Tm - temp[i]) * (cf.Tm > temp[i])) * (cf.T0 < temp[i])
+    trait[i] ~ dnorm(trait.mu[i], cf.tau)T(0,)
+    }
+
+    ## Derived Quantities and Predictions
+    for(i in 1:N.Temp.xs){
+    z.trait.mu.pred[i] <- cf.q * Temp.xs[i] * (Temp.xs[i] - cf.T0) * sqrt((cf.Tm - Temp.xs[i]) * (cf.Tm > Temp.xs[i])) * (cf.T0 < Temp.xs[i])
+    }
+
+    } # close model
+    ",fill=T)
+sink()
+
+# quadratic model (truncated)
+
+sink("quad_T.txt")
+cat("
+    model{
+
+    ## Priors
+    cf.T0 ~ dunif(0, 20)
+    cf.Tm ~ dunif(20, 45)
+    cf.q ~ dunif(0, 1)
+    cf.sigma ~ dunif(0, 1000)
+    cf.tau <- 1 / (cf.sigma * cf.sigma)
+
+    ## Likelihood
+    for(i in 1:N.obs){
+    trait.mu[i] <- -1 * cf.q * (temp[i] - cf.T0) * (temp[i] - cf.Tm) * (cf.Tm > temp[i]) * (cf.T0 < temp[i])
+    trait[i] ~ dnorm(trait.mu[i], cf.tau)T(0,)
+    }
+
+    ## Derived Quantities and Predictions
+    for(i in 1:N.Temp.xs){
+    z.trait.mu.pred[i] <- -1 * cf.q * (Temp.xs[i] - cf.T0) * (Temp.xs[i] - cf.Tm) * (cf.Tm > Temp.xs[i]) * (cf.T0 < Temp.xs[i])
+    }
+
+    } # close model
+    ",fill=T)
+sink()
