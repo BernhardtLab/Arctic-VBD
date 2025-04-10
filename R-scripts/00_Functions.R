043690a6b7809ed1722809caf2948ffb71543255
Lilian Chan <tszyinglilianchan@gmail.com>
2025-04-09 22:59:37 -0400

    Fit TPCs with uniform priors

--- R-scripts/00_Functions.R
+++ R-scripts/00_Functions.R
@@ -1,14 +1,17 @@
-setwd("~/Library/CloudStorage/OneDrive-UniversityofGuelph/Grad School/Chapter 1")
+setwd("~/Documents/UofG/Arctic-VBD/R-scripts/JAGS models")
 
-# Briere model (truncated)
+##########
+###### Briere model (truncated) ----
+##########
+
 sink("briere_T.txt")
 cat("
     model{
 
     ## Priors
+    cf.q ~ dunif(0, 1)
     cf.T0 ~ dunif(0, 20)
     cf.Tm ~ dunif(20, 45)
-    cf.q ~ dunif(0, 1)
     cf.sigma ~ dunif(0, 1000)
     cf.tau <- 1 / (cf.sigma * cf.sigma)
 
@@ -27,7 +30,40 @@ cat("
     ",fill=T)
 sink()
 
-# quadratic model (truncated)
+##########
+###### Briere Model with gamma priors (except sigma) ----
+##########
+
+sink("briere_inf.txt")
+cat("
+    model{
+    
+    ## Priors
+    cf.q ~ dgamma(hypers[1,1], hypers[2,1])
+    cf.T0 ~ dgamma(hypers[1,2], hypers[2,2])
+    cf.Tm ~ dgamma(hypers[1,3], hypers[2,3])
+    cf.sigma ~ dunif(0, 1000)
+    cf.tau <- 1 / (cf.sigma * cf.sigma)
+    
+    ## Likelihood
+    for(i in 1:N.obs){
+    trait.mu[i] <- cf.q * temp[i] * (temp[i] - cf.T0) * sqrt((cf.Tm - temp[i]) * (cf.Tm > temp[i])) * (cf.T0 < temp[i])
+    trait[i] ~ dnorm(trait.mu[i], cf.tau)
+    }
+    
+    ## Derived Quantities and Predictions
+    for(i in 1:N.Temp.xs){
+    z.trait.mu.pred[i] <- cf.q * Temp.xs[i] * (Temp.xs[i] - cf.T0) * sqrt((cf.Tm - Temp.xs[i]) * (cf.Tm > Temp.xs[i])) * (cf.T0 < Temp.xs[i])
+    }
+    
+    } # close model
+    ",fill=T)
+sink()
+
+
+##########
+###### Quadratic model (truncated) ----
+##########
 
 sink("quad_T.txt")
 cat("
--- R-scripts/02_TraitFits_MDR.R
+++ R-scripts/02_TraitFits_MDR.R
@@ -4,15 +4,27 @@
 ## Purpose: use Bayesian inference (JAGS) to fit TPCs for mosquito development 
 ## rate (MDR) for Aedes nigripes
 ##     1) with uniform priors; and 
-##     2) with informative priors using TPCs from Aedes sierrensis data
+##     2) with data-informed priors from Aedes sierrensis data
 ## 
 ## Table of content:
 ##    0. Set-up workspace
-##    1. Load and process data
-##    2. Fitting TPC
-##        A. Briere function (Truncated normally-distributed)
-##        B. Quadratic function (Truncated normally-distributed)
-##    3. Plotting
+##
+##    1. MCMC settings for all models
+##
+##    2. Fitting TPC (Briere)
+##        A. Fit MDR thermal responses with uniform priors (Ae. nigripes)
+##        B. Fit MDR thermal responses for priors (Ae. sierrensis)
+##        C. Fit gamma distributions to MDR prior thermal responses
+##        D. Fit MDR thermal responses with data-informed priors (Ae. nigripes)
+##
+##    3. Fitting TPC (Quadratic) (do this later - LC 2025.04.09)
+##        A. Fit MDR thermal responses with uniform priors (Ae. nigripes)
+##        B. Fit MDR thermal responses for priors (Ae. sierrensis)
+##        C. Fit gamma distributions to MDR prior thermal responses
+##        D. Fit MDR thermal responses with data-informed priors (Ae. nigripes)
+##
+##    4. Plotting
+
 
 
 ##########
@@ -30,10 +42,7 @@ library(broom)
 
 setwd("~/Documents/UofG/Arctic-VBD")
 
-##########
-###### 1. Load and process data ----
-##########
-
+# Load data
 data <- read_csv("data/data-processed/TraitData_MDR.csv")
 unique(data$species)
 
@@ -46,4 +55,74 @@ data %>% ggplot() +
   geom_point(aes(x = temp, y = trait, color = species), position = "jitter") +
   theme_bw()
 
+
+##########
+###### 1. model settings for all models ----
+##########
+
+##### inits Function
+inits<-function(){list(
+  cf.q = 0.01,
+  cf.Tm = 35,
+  cf.T0 = 5,
+  cf.sigma = rlnorm(1))}
+
+##### Parameters to Estimate
+parameters <- c("cf.q", "cf.T0", "cf.Tm","cf.sigma", "z.trait.mu.pred")
+
+##### MCMC Settings
+# Number of posterior dist elements = [(ni - nb) / nt ] * nc = [ (25000 - 5000) / 8 ] * 3 = 7500
+ni <- 110000 # number of iterations in each chain
+nb <- 10000 # number of 'burn in' iterations to discard
+nt <- 100 # thinning rate - jags saves every nt iterations in each chain
+nc <- 5 # number of chains
+
+##### Temp sequence for derived quantity calculations
+# For actual fits
+# Temp.xs <- seq(1, 45, 0.1)
+# N.Temp.xs <-length(Temp.xs)
+
+# For priors - fewer temps for derived calculations makes it go faster
+Temp.xs <- seq(5, 45, 0.5)
+N.Temp.xs <-length(Temp.xs)
+
+##########
+###### 2A. Fit MDR thermal responses with uniform priors (Ae. nigripes) ----
+##########
+
+##### Set data
+data <- data.MDR.nigripes
+
+##### Organize data for JAGS
+trait <- data$trait
+N.obs <- length(trait)
+temp <- data$temp
+
+##### define data for JAGS in a list object
+jag.data <- list(trait = trait, N.obs = N.obs, temp = temp, Temp.xs = Temp.xs, N.Temp.xs = N.Temp.xs)
+
+##### Run JAGS -----
+MDR.nigripes.bri.uni <- jags(data = jag.data, 
+                     inits = inits, 
+                     parameters.to.save = parameters, 
+                     model.file = "R-scripts/briere_T.txt",
+                     n.thin = nt, 
+                     n.chains = nc, 
+                     n.burnin = nb, 
+                     n.iter = ni, 
+                     DIC = T, 
+                     working.directory = getwd()
+)
+
+## Save the model as Rdata 
+#save(MDR.nigripes.bri.uni, file = "R-scripts/R2jags-objects/MDR.nigripes.bri.uni.Rdata")
+
+# Read the .Rdata
+#load("R-scripts/R2jags-objects/MDR.nigripes.bri.uni.Rdata")
+
+
+## Diagnostics ----
+##### Examine output
+MDR.nigripes.bri.uni$BUGSoutput$summary[1:5,]
+mcmcplot(MDR.nigripes.bri.uni)
 
