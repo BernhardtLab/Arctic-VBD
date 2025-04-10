f4fbc558a51f060780e72d94ca458559b71e4b10
Lilian Chan <tszyinglilianchan@gmail.com>
2025-04-10 15:07:44 -0400

    Finished fitting MDR TPCs

--- R-scripts/02_TraitFits_MDR.R
+++ R-scripts/02_TraitFits_MDR.R
@@ -16,6 +16,7 @@
 ##        B. Fit MDR thermal responses for priors (Ae. sierrensis)
 ##        C. Fit gamma distributions to MDR prior thermal responses
 ##        D. Fit MDR thermal responses with data-informed priors (Ae. nigripes)
+##        E. Plot all three TPCs in the same graph (for comparison)
 ##
 ##    3. Fitting TPC (Quadratic) (do this later - LC 2025.04.09)
 ##        A. Fit MDR thermal responses with uniform priors (Ae. nigripes)
@@ -36,9 +37,8 @@ library(readxl)
 library(janitor)
 library(R2jags)
 library(mcmcplots) # Diagnostic plots for fits
-library(rTPC)
-library(nls.multstart)
-library(broom)
+library(MASS)
+library(ggsci)
 
 setwd("~/Documents/UofG/Arctic-VBD")
 
@@ -71,7 +71,7 @@ inits<-function(){list(
 parameters <- c("cf.q", "cf.T0", "cf.Tm","cf.sigma", "z.trait.mu.pred")
 
 ##### MCMC Settings
-# Number of posterior dist elements = [(ni - nb) / nt ] * nc = [ (25000 - 5000) / 8 ] * 3 = 7500
+# Number of posterior dist elements = [(ni - nb) / nt ] * nc = [ (110000 - 10000) / 100 ] * 5 = 5000
 ni <- 110000 # number of iterations in each chain
 nb <- 10000 # number of 'burn in' iterations to discard
 nt <- 100 # thinning rate - jags saves every nt iterations in each chain
@@ -79,11 +79,11 @@ nc <- 5 # number of chains
 
 ##### Temp sequence for derived quantity calculations
 # For actual fits
-# Temp.xs <- seq(1, 45, 0.1)
+# Temp.xs <- seq(0, 45, 0.1)
 # N.Temp.xs <-length(Temp.xs)
 
 # For priors - fewer temps for derived calculations makes it go faster
-Temp.xs <- seq(5, 45, 0.5)
+Temp.xs <- seq(0, 45, 0.5)
 N.Temp.xs <-length(Temp.xs)
 
 ##########
@@ -102,23 +102,23 @@ temp <- data$temp
 jag.data <- list(trait = trait, N.obs = N.obs, temp = temp, Temp.xs = Temp.xs, N.Temp.xs = N.Temp.xs)
 
 ##### Run JAGS -----
-MDR.nigripes.bri.uni <- jags(data = jag.data, 
-                     inits = inits, 
-                     parameters.to.save = parameters, 
-                     model.file = "R-scripts/briere_T.txt",
-                     n.thin = nt, 
-                     n.chains = nc, 
-                     n.burnin = nb, 
-                     n.iter = ni, 
-                     DIC = T, 
-                     working.directory = getwd()
-)
+# MDR.nigripes.bri.uni <- jags(data = jag.data, 
+#                      inits = inits, 
+#                      parameters.to.save = parameters, 
+#                      model.file = "R-scripts/briere_T.txt",
+#                      n.thin = nt, 
+#                      n.chains = nc, 
+#                      n.burnin = nb, 
+#                      n.iter = ni, 
+#                      DIC = T, 
+#                      working.directory = getwd()
+# )
 
 ## Save the model as Rdata 
 #save(MDR.nigripes.bri.uni, file = "R-scripts/R2jags-objects/MDR.nigripes.bri.uni.Rdata")
 
 # Read the .Rdata
-#load("R-scripts/R2jags-objects/MDR.nigripes.bri.uni.Rdata")
+load("R-scripts/R2jags-objects/MDR.nigripes.bri.uni.Rdata")
 
 
 ## Diagnostics ----
@@ -126,3 +126,253 @@ MDR.nigripes.bri.uni <- jags(data = jag.data,
 MDR.nigripes.bri.uni$BUGSoutput$summary[1:5,]
 mcmcplot(MDR.nigripes.bri.uni)
 
+# Extract the DIC for future model comparisons
+MDR.nigripes.bri.uni$BUGSoutput$DIC
+
+## Plot data + fit ----
+df.MDR.nigripes.bri.uni <- data.frame(MDR.nigripes.bri.uni$BUGSoutput$summary)[-(1:5),] %>% 
+  mutate(temp = Temp.xs) %>% # Add the corresponding temp to the dataframe
+  dplyr::select(temp, mean, sd, X2.5., X97.5.)
+
+head(df.MDR.nigripes.bri.uni)
+
+##### Plot
+plot.MDR.nigripes.bri.uni <- df.MDR.nigripes.bri.uni %>% 
+  ggplot(aes(x = temp)) +
+  geom_ribbon(aes(ymin = X2.5., ymax = X97.5.), fill = "#4363d8", alpha = 0.5) +
+  geom_line(aes(y = mean), color = "blue", linewidth = 1) +
+  geom_point(data = data, aes(x = temp, y = trait), size = 2) +
+  # Customize the axes and labels
+  #scale_x_continuous(limits = c(0, 41)) + 
+  #scale_y_continuous(limits = c(-0.005, 0.19)) +
+  labs(
+    x = expression(paste("Temperature (", degree, "C)")),
+    y = "Development rate (days-1)"
+  ) +
+  theme_bw()
+
+plot.MDR.nigripes.bri.uni
+
+# ggsave("figures/MDR.nigripes.bri.uni.png", plot.MDR.nigripes.bri.uni, 
+#        width = 10.3, height = 5.6)
+
+##########
+###### 2B. Fit MDR thermal responses for priors (Ae. sierrensis) ----
+##########
+
+##### Set data
+data <- data.MDR.sierrensis
+
+##### Organize data for JAGS
+trait <- data$trait
+N.obs <- length(trait)
+temp <- data$temp
+
+##### define data for JAGS in a list object
+jag.data <- list(trait = trait, N.obs = N.obs, temp = temp, Temp.xs = Temp.xs, N.Temp.xs = N.Temp.xs)
+
+##### Run JAGS -----
+
+# This code took an hour to run!
+# MDR.sierrensis.bri.uni <- jags(data = jag.data, 
+#                              inits = inits, 
+#                              parameters.to.save = parameters, 
+#                              model.file = "R-scripts/briere_T.txt",
+#                              n.thin = nt, 
+#                              n.chains = nc, 
+#                              n.burnin = nb, 
+#                              n.iter = ni, 
+#                              DIC = T, 
+#                              working.directory = getwd()
+# )
+
+## Save the model as Rdata 
+#save(MDR.sierrensis.bri.uni, file = "R-scripts/R2jags-objects/MDR.sierrensis.bri.uni.Rdata")
+
+# Read the .Rdata
+load("R-scripts/R2jags-objects/MDR.sierrensis.bri.uni.Rdata")
+
+
+## Diagnostics ----
+##### Examine output
+MDR.sierrensis.bri.uni$BUGSoutput$summary[1:5,]
+mcmcplot(MDR.sierrensis.bri.uni)
+
+# Extract the DIC for future model comparisons
+MDR.sierrensis.bri.uni$BUGSoutput$DIC
+
+## Plot data + fit ----
+df.MDR.sierrensis.bri.uni <- data.frame(MDR.sierrensis.bri.uni$BUGSoutput$summary)[-(1:5),] %>% 
+  mutate(temp = Temp.xs) %>% # Add the corresponding temp to the dataframe
+  dplyr::select(temp, mean, sd, X2.5., X97.5.)
+
+head(df.MDR.sierrensis.bri.uni)
+
+##### Plot
+plot.df.MDR.sierrensis.bri.uni <- df.MDR.sierrensis.bri.uni %>% 
+  ggplot(aes(x = temp)) +
+  geom_ribbon(aes(ymin = X2.5., ymax = X97.5.), fill = "grey", alpha = 0.5) +
+  geom_line(aes(y = mean), color = "#868686FF", linewidth = 1) +
+  geom_point(data = data, aes(x = temp, y = trait), size = 2 
+             , position = "jitter"
+             ) +
+  # Customize the axes and labels
+  #scale_x_continuous(limits = c(0, 41)) + 
+  #scale_y_continuous(limits = c(-0.005, 0.19)) +
+  labs(
+    x = expression(paste("Temperature (", degree, "C)")),
+    y = "Development rate (days-1)"
+  ) +
+  theme_bw()
+
+plot.df.MDR.sierrensis.bri.uni
+
+# ggsave("figures/MDR.sierrensis.bri.uni.png", plot.df.MDR.sierrensis.bri.uni, 
+#        width = 10.3, height = 5.6)
+
+
+##########
+###### 2C. Fit gamma distributions to MDR prior thermal responses ----
+##########
+
+# Get the posterior dists for 3 main parameters (not sigma) into a data frame
+MDR.nigripes.prior.cf.dists <- data.frame(q = as.vector(MDR.sierrensis.bri.uni$BUGSoutput$sims.list$cf.q),
+                                          T0 = as.vector(MDR.sierrensis.bri.uni$BUGSoutput$sims.list$cf.T0),
+                                          Tm = as.vector(MDR.sierrensis.bri.uni$BUGSoutput$sims.list$cf.Tm))
+
+# Fit gamma distributions for each parameter posterior dists
+MDR.nigripes.prior.gamma.fits = apply(MDR.nigripes.prior.cf.dists, 2, 
+                                      function(df) fitdistr(df, "gamma")$estimate)
+
+
+MDR.hypers <- MDR.nigripes.prior.gamma.fits
+save(MDR.hypers, file = "R-scripts/R2jags-objects/MDRhypers.Rsave")
+
+
+##########
+###### 2D. Fit MDR thermal responses with data-informed priors (Ae. nigripes) ----
+##########
+
+load("R-scripts/R2jags-objects/MDRhypers.Rsave")
+MDR.nigripes.prior.gamma.fits <- MDR.hypers
+
+##### Temp sequence for derived quantity calculations
+# For actual fits
+Temp.xs <- seq(0, 45, 0.1)
+N.Temp.xs <-length(Temp.xs)
+
+##### Set data
+data <- data.MDR.nigripes
+hypers <- MDR.nigripes.prior.gamma.fits * 0.1
+
+##### Organize data for JAGS
+trait <- data$trait
+N.obs <- length(trait)
+temp <- data$temp
+
+##### define data for JAGS in a list object
+jag.data <- list(trait = trait, N.obs = N.obs, temp = temp, Temp.xs = Temp.xs, 
+                 N.Temp.xs = N.Temp.xs, hypers = hypers)
+
+##### Run JAGS -----
+# MDR.nigripes.bri.inf <- jags(data = jag.data, 
+#                              inits = inits, 
+#                              parameters.to.save = parameters, 
+#                              model.file = "R-scripts/briere_inf.txt",
+#                              n.thin = nt, 
+#                              n.chains = nc, 
+#                              n.burnin = nb, 
+#                              n.iter = ni, 
+#                              DIC = T, 
+#                              working.directory = getwd()
+# )
+
+## Save the model as Rdata 
+#save(MDR.nigripes.bri.uni, file = "R-scripts/R2jags-objects/MDR.nigripes.bri.inf.Rdata")
+
+# Read the .Rdata
+load("R-scripts/R2jags-objects/MDR.nigripes.bri.inf.Rdata")
+
+
+## Diagnostics ----
+##### Examine output
+MDR.nigripes.bri.inf$BUGSoutput$summary[1:5,]
+mcmcplot(MDR.nigripes.bri.inf)
+
+# Extract the DIC for future model comparisons
+MDR.nigripes.bri.inf$BUGSoutput$DIC
+
+## Plot data + fit ----
+df.MDR.nigripes.bri.inf <- data.frame(MDR.nigripes.bri.inf$BUGSoutput$summary)[-(1:5),] %>% 
+  mutate(temp = Temp.xs) %>% # Add the corresponding temp to the dataframe
+  dplyr::select(temp, mean, sd, X2.5., X97.5.)
+
+head(df.MDR.nigripes.bri.inf)
+
+##### Plot
+plot.MDR.nigripes.bri.inf <- df.MDR.nigripes.bri.inf %>% 
+  ggplot(aes(x = temp)) +
+  geom_ribbon(aes(ymin = X2.5., ymax = X97.5.), fill = "pink", alpha = 0.5) +
+  geom_line(aes(y = mean), color = "red", linewidth = 1) +
+  geom_point(data = data, aes(x = temp, y = trait), size = 2) +
+  # Customize the axes and labels
+  #scale_x_continuous(limits = c(0, 41)) + 
+  #scale_y_continuous(limits = c(-0.005, 0.19)) +
+  labs(
+    x = expression(paste("Temperature (", degree, "C)")),
+    y = "Development rate (days-1)"
+  ) +
+  theme_bw()
+
+plot.MDR.nigripes.bri.inf
+
+# ggsave("figures/MDR.nigripes.bri.inf.png", plot.MDR.nigripes.bri.inf, 
+#        width = 10.3, height = 5.6)
+
+
+##########
+###### 2E. Plot all three TPCs in the same graph (for comparison) ----
+##########
+
+# Add an identifying column in each model output dataframe
+df.MDR.nigripes.bri.uni <- df.MDR.nigripes.bri.uni %>% 
+  mutate(type = "Ae. nigripes uniform")
+
+df.MDR.sierrensis.bri.uni <- df.MDR.sierrensis.bri.uni %>% 
+  mutate(type = "Ae. sierrensis uniform")
+
+df.MDR.nigripes.bri.inf <- df.MDR.nigripes.bri.inf %>% 
+  mutate(type = "Ae. nigripes informative")
+
+# Combine the three dataframes
+df.all <- rbind(df.MDR.nigripes.bri.uni, df.MDR.sierrensis.bri.uni, df.MDR.nigripes.bri.inf)
+
+# Plot
+plot.all <- df.all %>% 
+  ggplot(aes(x = temp)) +
+  geom_ribbon(aes(ymin = X2.5., ymax = X97.5., fill = type), alpha = 0.5) +
+  geom_line(aes(y = mean, color = type), linewidth = 1) +
+  geom_point(data = data.MDR.nigripes, aes(x = temp, y = trait), size = 2) +
+  #geom_point(data = data.MDR.sierrensis, aes(x = temp, y = trait), size = 2) +
+  # Customize the axes and labels
+  #scale_x_continuous(limits = c(0, 41)) + 
+  #scale_y_continuous(limits = c(-0.005, 0.19)) +
+  labs(
+    x = expression(paste("Temperature (", degree, "C)")),
+    y = "Development rate (days-1)"
+  ) +
+  # Customize the colours
+  ## ribbon
+  scale_fill_manual(values = c("Ae. nigripes uniform" = "#4363d8", 
+                             "Ae. sierrensis uniform" = "grey",
+                             "Ae. nigripes informative" = "pink")) +
+  ## line
+  scale_color_manual(values = c("Ae. nigripes uniform" = "blue", 
+                               "Ae. sierrensis uniform" = "#868686FF",
+                               "Ae. nigripes informative" = "red")) +
+  theme_bw()
+
+plot.all
+
+#ggsave("figures/MDR.all.png", plot.all, 
+#        width = 10.3, height = 5.6)
